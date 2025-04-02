a21f2512b54dbebb65fed7edb0f85361655e220f
Michael Mammino <famigliamammino5@libero.it>
2025-04-02 20:00:06 +0200

    cambiato in inglese

--- traccia3/traccia3.js
+++ traccia3/traccia3.js
@@ -2,7 +2,7 @@ var currentYear=2025
 var yearOfBirth=1999
 
 var yourYear=currentYear-yearOfBirth
-var 100Year=100-yourYear
+var missingYears=100-yourYear
 
 console.log('la tua età è di'+' '+ yourYear +' '+'anni')
-console.log('ti mancano'+' '+ 100Year +' '+'anni ad averne 100')
+console.log('ti mancano'+' '+ missingYears +' '+'anni ad averne 100')
